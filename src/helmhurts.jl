b3d14439a614ecdc48c8f9262798d6ab5aac35ad
Matthias Wilhelm <mwilhelm.kl@gmail.com>
2015-02-09 12:14:11 +0100

    fixed the indention, oneroom setting

--- src/helmhurts.jl
+++ src/helmhurts.jl
@@ -2,19 +2,19 @@ using Color
 using Images
 using ImageView
 
-const δ = 0.02                # one pixel in the floor plan equals δ meters
+const δ = 0.01                      # one pixel in the floor plan equals δ meters
 
-const n_air = 1.                      # refractive index for air
-const n_concrete = 2.12 - 0.021im     # refractive index for concrete
-                                      # the imaginary part conveys the absorption
+const n_air = 1.                    # refractive index for air
+const n_concrete = 2.12 - 0.021im   # refractive index for concrete
+                                    # the imaginary part conveys the absorption
 
-const λ = 0.12     # for a 2.5 GHz signal, wavelength is ~ 12cm
-const k = 2π/λ     # k is the wavenumber
+const λ = 0.12                      # for a 2.5 GHz signal, wavelength is ~ 12cm
+const k = 2π/λ                      # k is the wavenumber
 
-const infile = "resources/plan3.png"
+const infile = "resources/oneroom.png"
 
-const txX = 400
-const txY = 550
+const txX = 720
+const txY = 450
 
 function generateMu(filename)
 	img = imread(filename)
@@ -22,7 +22,7 @@ function generateMu(filename)
 
 	μ = similar(plan, Complex)
 	μ[plan .== 255] = (k/n_air)^2            # white signifies empty space
-  μ[plan .≠  255] = (k/n_concrete)^2       # everything else are obstactles
+	μ[plan .≠  255] = (k/n_concrete)^2       # everything else are obstactles
 
 	return μ
 end
@@ -53,7 +53,7 @@ function generateMatrix(μ)
 			if 1<x<dimx && 1<y<dimy
 				vs[i] = δ^-2
 			else
-				vs[i] = 1000.0 # FIXME: what should happen when the matrix hits the boundaries?
+				vs[i] = 1e6 # FIXME: what should happen when the matrix hits the boundaries?
 			end
 
 			i += 1
@@ -65,6 +65,8 @@ end
 
 function plotMatrix(A, outfile)
 	E = 20*log10(real(A) .* real(A))
+	writecsv("test.csv", E .- maximum(E))
+
 	Ei = round(Integer, min(100, max(1, (int(1 .+ 100 .* (E .- minimum(E))/(maximum(E) - minimum(E)))))))
 
 	cm = colormap("oranges", logscale=true)
@@ -88,19 +90,19 @@ end
 ## -------------------------------------------
 
 function main()
-  println("Starting operation …")
+	println("Starting operation …")
 	μ = generateMu(infile)
 	S = generateMatrix(μ)
 
 	for movex in (txX,)#1381:20:1420
 		f = zeros(Complex, size(μ))
-		f[movex, txY] = 1.0;              # our Wifi emitter antenna will be there;
+		f[movex, txY] = 1e4              # our Wifi emitter antenna will be there;
 
-    println("Solving the matrix equation …")
-		A = reshape(S \ vec(f), size(μ)...);
+	println("Solving the matrix equation …")
+		A = reshape(S \ vec(f), size(μ)...)
 
-    println("Plotting stuff …")
-		plotMatrix(A, "figs/h-$(lpad(txX, 4, '0'))x$(lpad(txY, 4, '0')).png")
+	println("Plotting stuff …")
+		plotMatrix(A, "figs/oneroom/h-$(lpad(txX, 4, '0'))x$(lpad(txY, 4, '0')).png")
 	end
 end
 
