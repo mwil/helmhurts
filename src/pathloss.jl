077aa71a4ea18648b4a90f594a0f2fb8126b829d
Matthias Wilhelm <mwilhelm.kl@gmail.com>
2015-02-11 12:36:29 +0100

    switched to some matrix magic for pathloss

--- src/pathloss.jl
+++ src/pathloss.jl
@@ -2,34 +2,33 @@ using Color
 using Images
 
 const INFILE = "resources/floorplan-wf.png"
+const N_COLORS = 10
 
-const TX_POS = 870, 425
-
-dist(x,y) = sqrt((TX_POS[1]-x)^2 + (TX_POS[2]-y)^2) * 0.2
+const TX_POS    = 870, 425
+const TX_POS_IM = 870 + 425im
 
 img = imread(INFILE)
 plan = reinterpret(Uint8, data(img));
 
 dimx, dimy = size(plan)
 
-E = similar(plan, Float64)
+Dist = similar(plan, Float64)
 
 for x in 1:dimx, y in 1:dimy
-	if (x,y) == TX_POS
-		E[x,y] = 20*log10(dist(x-1,y-1)^-2)
-	else
-		E[x,y] = 20*log10(dist(x,y)^-2)
-	end
+	Dist[x,y] = 0.2*abs(x+y*im - TX_POS_IM)
 end
 
-#E[E .< -90] = -90
+Dist[TX_POS...] = Dist[TX_POS[1]-1, TX_POS[2]-1]
 
-cm = reverse(colormap("blues", 10))
-Ei = round(Integer, min(10, max(1, (int(1 .+ 10 .* (E .- minimum(E))/(maximum(E) - minimum(E)))))))
+E = similar(Dist, Float64)
+E = 20 .* log10(Dist .^ -2)
 
-img = imread(INFILE)
-plan = reinterpret(Uint8, data(img));
-Ei[plan .== 0] = 10;
+E[E .< -90] = -90
+
+cm = reverse(colormap("blues", N_COLORS))
+Ei = round(Integer, min(N_COLORS, max(1, (int(1 .+ N_COLORS .* (E .- minimum(E))/(maximum(E) - minimum(E)))))))
+
+Ei[plan .== 0x00] = N_COLORS;   # draw the walls
 
 field = Array(Float64, (size(Ei)[1], size(Ei)[2], 3))
 field[:,:,1] = [ cm[ei].r for ei in Ei ]
